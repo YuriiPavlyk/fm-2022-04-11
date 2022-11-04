35112a5daf0465565a78de047f9c6ddc6a62f16c
yurii <pavlykyurii15@gmail.ru>
2022-11-04 23:40:30 +0200

    Interchangeable

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-alert(550)
+console.log(550)
+console.log(234)
+console.log('hello')
+console.log(false)
+console.log(null)
+console.log(undefined)
+
+console.log(userFirstname)
+
+let userFirstname = 'Tom';
+userFirstname ='Bred';
+userFirstname ='O\'Donel';
+
+console.log(userFirstname)
+console.log(550)
+console.log(234)
+console.log('hello')
+console.log(false)
+console.log(null)
+console.log(undefined)
+
+let isSunInSkyNow ='false'
